fe4b50d699cda0635e0e78313a40e0630cb9032d
JamesYou <james.you@l2political.com>
2020-04-02 20:05:52 -0700

    put everything in df, normalize

--- save_matchdata.py
+++ save_matchdata.py
@@ -2,26 +2,48 @@ import pandas as pd
 from pandas.io.json import json_normalize    
 import json
 import datetime
+import configparser
+import json
+import os
+import requests
 
-patch_time = datetime.datetime.now() - datetime.timedelta(days=365)
-with open('data/raw-games/na1/NA1_3353423535.json') as data_file:    
-    data = json.load(data_file)
+from dateutil import parser
 
-s=data['game_version']
-date_time_str=s[s.find("(")+1:s.find(")")]
-date_time_obj = datetime.datetime.strptime(date_time_str, '%b %d %Y/%H:%M:%S')
-if date_time_obj > patch_time:
-    patch_time = date_time_obj
-if patch_time > date_time_obj:
-    continue
+config = configparser.ConfigParser()
+config.read('config.ini')
+regions = config.get('adjustable', 'regions').split(',')
 
-participants = json_normalize(data,record_path=['participants'])
-traits = json_normalize(data, 
+allrecords = []
+for region in regions:
+    mintime = datetime.datetime.now() - datetime.timedelta(days=14)
+    for name in os.listdir(config.get('setup', 'raw_data_dir') + '/{}'.format(region)):
+        with open(config.get('setup', 'raw_data_dir') + '/{}/{}'.format(region, name), 'r', encoding="utf-8") as file: 
+            data = json.load(file)
+            s=data['game_version']
+            date_time_str=s[s.find("(")+1:s.find(")")]
+            date_time_obj = datetime.datetime.strptime(date_time_str, '%b %d %Y/%H:%M:%S')
+            if mintime > date_time_obj:
+                continue
+            if data['queue_id'] != 1100:
+                continue
+            data.update({'match_id':name})
+            allrecords.append(data)
+
+#get traits and units
+traits = json_normalize(allrecords, 
 record_path=['participants','traits'],
-meta=[['participants','placement']])
+meta=['match_id',['participants','placement'],['participants','puuid']])
 
-champions = json_normalize(data,
-record_path=['participants','champions'],
-meta=['placement','puuid'])
+units = json_normalize(allrecords,
+record_path=['participants','units'],
+meta=['match_id',['participants','placement'],['participants','puuid']])
 
-pd.show_versions()
+unitspivot=pd.pivot_table(units,index=['match_id','participants.placement'], columns='character_id',values='tier')
+traitspivot=pd.pivot_table(traits,index=['match_id','participants.placement'], columns='name',values='tier_current')
+
+combinepivot = unitspivot.join(traitspivot).reset_index()
+
+df=json_normalize(allrecords)
+df=df.loc[df['game_version']==df['game_version'].max()]
+
+combinepivot.merge(df,on='match_id')[combinepivot.columns]
