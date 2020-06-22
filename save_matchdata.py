7999909a3ace548f3a8b3d3872d0bcd765cd20ea
JamesYou <james.you@l2political.com>
2020-06-22 15:52:32 -0700

    add comments, swap shit around

--- save_matchdata.py
+++ save_matchdata.py
@@ -5,33 +5,40 @@ import pandas as pd
 import hdbscan
 import numpy as np
 from TFTClusterer import TFTClusterer
-from save_matchdata import loaddb
 from datetime import datetime, timedelta
 from googledrivesave import trashfolder, outputtodrive
 import pygsheets
 
 def main():
-    df = loaddb(timestamp=(datetime.now() - timedelta(hours=4)).timestamp()*1000)
+    #Grab Data
+    df = loaddb(timestamp=(datetime.now() - timedelta(hours=24)).timestamp()*1000)
     gameversion = df['game_version'].max()
 
     df=df.loc[df['game_version']==gameversion]
 
     assert len(df) >= 100, "less than 100 matches in newest patch"
 
+    #Cluster Data
     clusterclass=TFTClusterer(df)
-
     clusterclass.cluster(divisor=25)
 
+    #output Files for Power BI
     clusterclass.unitshdb.to_csv("unitshdb.csv",index=False)
     clusterclass.itemshdb.to_csv("itemshdb.csv",index=False)
     clusterclass.clusterdf[["comp_id","participants.placement","hdb","game_variation"]].to_csv("hdb.csv",index=False)
     
+    #Write newest date
+    f = open('newestdate.csv','w')
+    f.write(str(datetime.fromtimestamp(df['game_datetime'].max()/1e3))) #Give your csv text here.
+    f.close()
+
+    #Output files to drive for deracher
     allhdbdf = clusterclass.allhdbdf()
 
     variationname = 'AllGalaxies'
+    trashfolder()
 
     outputtodrive(allhdbdf.sort_index(),variationname)
-    str(datetime.fromtimestamp(df['game_datetime'].max()/1e3))
 
     outputtodrive(pd.DataFrame(data={'last_datetime': [df['game_datetime'].max()]}),'last_datetime')
     outputtodrive(pd.DataFrame(df['game_variation'].value_counts()),'gamevariationcounts')
