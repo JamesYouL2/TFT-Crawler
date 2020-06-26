92f797400fedcc587dd9c7d9a2f16bf14e5e04d8
JamesYou <james.you@l2political.com>
2020-06-25 20:57:01 -0700

    output figure analyzing data

--- save_matchdata.py
+++ save_matchdata.py
@@ -9,6 +9,8 @@ from datetime import datetime, timedelta
 from googledrivesave import trashfolder, outputtodrive
 import pygsheets
 from SQLGatherer import loaddb
+from matplotlib import pyplot as plt
+
 
 def main():
     #Grab Data
@@ -23,11 +25,16 @@ def main():
     clusterclass=TFTClusterer(df)
     clusterclass.cluster(divisor=25)
 
+    #Output cluster figure
+    clusterclass.plot.figure.savefig('fig.png')
+    pd.DataFrame(clusterclass.clusterdf.groupby('hdbnumber')['hdb'].value_counts()).to_csv('hdbnumber.csv')
+
     #output Files for Power BI
     clusterclass.unitshdb.to_csv("unitshdb.csv",index=False)
     clusterclass.itemshdb.to_csv("itemshdb.csv",index=False)
     clusterclass.clusterdf[["comp_id","participants.placement","hdb","game_variation"]].to_csv("hdb.csv",index=False)
-    
+
+
     #Write newest date
     f = open('newestdate.csv','w')
     f.write(str(datetime.fromtimestamp(df['game_datetime'].max()/1e3))) #Give your csv text here.
