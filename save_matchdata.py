39767cc6121b067d9cd518309b3e06418e2594e6
JamesYou <james.you@l2political.com>
2020-07-21 23:18:11 -0700

    switch back divisor

--- save_matchdata.py
+++ save_matchdata.py
@@ -24,7 +24,7 @@ def main():
     #Cluster Data
     clusterclass=TFTClusterer(df)
     #clusterclass.imputetraits()
-    clusterclass.cluster(divisor=18)
+    clusterclass.cluster(divisor=25)
 
     #Output cluster figure
     clusterclass.plot.figure.savefig('fig.png')
