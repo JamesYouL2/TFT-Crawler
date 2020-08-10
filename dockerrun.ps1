38a2fecaad5f79f41b8849a87a9a72385d3e9fff
JamesYou <james.you@l2political.com>
2020-08-09 17:48:34 -0700

    add dns to jupyter app

--- dockerrun.ps1
+++ dockerrun.ps1
@@ -1,4 +1,4 @@
 #1. Build Dockerfile
 docker build . -f Dockerfile -t tft-crawler
 #2. Docker Run
-docker run --rm -it -v ${PWD}:/app tft-crawler
+docker run --dns 8.8.8.8 --rm -it -v ${PWD}:/app tft-crawler 
