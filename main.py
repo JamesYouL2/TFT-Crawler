ab18b5d0317206c6721e9e4734c5dcbbe6bad82f
JamesYouL2 <james.you@l2political.com>
2020-08-18 16:40:35 -0700

    use different loading infrastructure

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ import time
 
 #new Workflow
 start=time.time()
-print ("Get puuid")
-asyncio.run(loadpuuid.main())
-print("GetpuuidTime:",(time.time()-start)/60)
-
-start=time.time()
-print ("Get matchhistory")
-asyncio.run(loadmatchhistory.main())
-print("GetMatchHistoryTime:",(time.time()-start)/60)
-
-start=time.time()
 print ("Create Clusters")
 save_matchdata.main()
 print("GetClusterTime:",(time.time()-start)/60)
