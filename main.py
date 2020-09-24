b75abb1529f4b3071773a74658bdfae606115b85
JamesYouL2 <james.you@l2political.com>
2020-09-23 21:17:11 -0700

    Make params more explicit

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ import time
 #new Workflow
 start=time.time()
 print ("Create Clusters")
-save_matchdata.main(hours=24*7, divisor = 50)
+save_matchdata.main(hours=24*7, divisor = 50, minunit=1.25)
 print("GetClusterTime:",(time.time()-start)/60)
